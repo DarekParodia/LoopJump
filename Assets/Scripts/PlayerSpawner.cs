60592551defb67f7997c6c1761d004a276b859b7
DarekParodia <darekparodia@gmail.com>
2026-03-13 17:26:33 +0100

    spawner working

--- Assets/Scripts/PlayerSpawner.cs
+++ Assets/Scripts/PlayerSpawner.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 
 public class PlayerSpawner : MonoBehaviour
 {
+    [SerializeField] private GameObject playerPrefab;
+    [SerializeField] private bool spawnPlayerOnStart = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if (spawnPlayerOnStart)
+            {
+            SpawnPlayer();
+            }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SpawnPlayer()
     {
-        
+        Instantiate(playerPrefab, transform.position, Quaternion.identity);
     }
 }
